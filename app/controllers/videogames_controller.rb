2e3372cb55b66271b4590a971975a05f16ca982f
Steen Clarke <cmclarkey@gmail.com>
2016-08-17 21:31:48 -0700

    Make post request on new form to api

--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -8,4 +8,18 @@ class VideogamesController < ApplicationController
     @videogame = Unirest.get("http://localhost:3000/api/v1/videogames/#{params[:id]}.json").body
   end
 
+  def new
+  end
+
+  def create
+    @videogame = Unirest.post("http://localhost:3000/api/v1/videogames.json",
+      headers:{ "Accept" => "application/json" },
+      parameters:{:name => params[:name],
+        :release_date => params[:release_date],
+        :genre => params[:genre],
+        :gameplay => params[:gameplay],
+        :steen_rating => params[:steen_rating]})
+    redirect_to '/videogames'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/videogames/new', to: 'videogames#new'
+  post '/videogames', to: 'videogames#create'
   get '/videogames/:id', to: 'videogames#show'
   get '/', to: 'videogames#index'
   get '/videogames', to: 'videogames#index'
