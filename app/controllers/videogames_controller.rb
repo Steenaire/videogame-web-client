dbf3102e4d83c68060b83c661942a06773a38c1b
Steen Clarke <cmclarkey@gmail.com>
2016-08-18 20:55:03 -0700

    Add dotenv gem, add domain to environmental variables

--- app/controllers/videogames_controller.rb
+++ app/controllers/videogames_controller.rb
@@ -1,18 +1,18 @@
 class VideogamesController < ApplicationController
 
   def index
-    @videogames = Unirest.get("http://localhost:3000/api/v1/videogames.json").body
+    @videogames = Unirest.get("#{ENV['DOMAIN']}/videogames.json").body
   end
 
   def show
-    @videogame = Unirest.get("http://localhost:3000/api/v1/videogames/#{params[:id]}.json").body
+    @videogame = Unirest.get("#{ENV['DOMAIN']}/videogames/#{params[:id]}.json").body
   end
 
   def new
   end
 
   def create
-    @videogame = Unirest.post("http://localhost:3000/api/v1/videogames.json",
+    @videogame = Unirest.post("#{ENV['DOMAIN']}/videogames.json",
       parameters:{name: params[:name],
       release_date: params[:release_date],
       genre: params[:genre],
@@ -23,11 +23,11 @@ class VideogamesController < ApplicationController
   end
 
   def edit
-    @videogame = Unirest.get("http://localhost:3000/api/v1/videogames/#{params[:id]}.json").body
+    @videogame = Unirest.get("#{ENV['DOMAIN']}/videogames/#{params[:id]}.json").body
   end
 
   def update
-    @videogame = Unirest.patch("http://localhost:3000/api/v1/videogames/#{params[:id]}.json",
+    @videogame = Unirest.patch("#{ENV['DOMAIN']}/videogames/#{params[:id]}.json",
       parameters:{name: params[:name],
       release_date: params[:release_date],
       genre: params[:genre],
@@ -38,7 +38,7 @@ class VideogamesController < ApplicationController
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v1/videogames/#{params[:id]}.json").body
+    Unirest.delete("#{ENV['DOMAIN']}/videogames/#{params[:id]}.json").body
     redirect_to '/videogames'
   end
 
